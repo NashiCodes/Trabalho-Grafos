8469d0806fc14e6c0cede729336460d166f5bafc
Perera Dev <nashi.codes@gmail.com>
2024-12-27 15:32:23 -0300

    Adiciona @brief

--- src/structures/storage/Matrices/Matrix.h
+++ src/structures/storage/Matrices/Matrix.h
@@ -6,6 +6,13 @@
 #define MATRIX_H
 #include "../Storage.h"
 
+/**
+ * @brief Classe para armazenamento de dados em forma de matriz
+ * Seguir o exemplo de implementação no meu repositório https://github.com/NashiCodes/DataStructs
+ * Lá contem um exemplo de implementação de uma matriz comum e uma matriz linha
+ * usar o mesmo conceito de template para as duas classes
+ * @tparam T Tipo de dado a ser armazenado, como arestas ou vertices
+ */
 template<class T>
 class Matrix final : public Storage<T> {
 private:
--- src/structures/storage/Storage.h
+++ src/structures/storage/Storage.h
@@ -6,14 +6,18 @@
 #define STORAGE_H
 
 
-
+/**
+ * @brief Interface para diferentes tipos de armazenamento, como listas, matrizes
+ *
+ * @tparam T Tipo de dado a ser armazenado, como arestas ou vertices
+ */
 template<class T>
-class Storage final {
+class Storage {
 public:
     virtual ~Storage() = default;
 
     virtual void add(T info){};
-    virtual T get(int ID){}
+    virtual T get(int ID) { return nullptr; }
 };
 
 
